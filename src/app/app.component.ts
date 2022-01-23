850b8cd76ccf619ab5ce69431abc40211c2b97a3
mj <jaczewski147@gmail.com>
2022-01-23 20:43:06 +0100

    add win popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { InfoDialogData } from './models/info-dialog-data';
 import { InfoPopupComponent } from './components/info-popup/info-popup.component';
 import { Component, HostListener, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AnimationSegment, Circle, Path } from './models/animation';
 import { GuessingLetter } from './models/letter';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import * as moment from 'moment';
 
 @Component({
 	selector: 'app-root',
@@ -20,17 +20,22 @@ export class AppComponent implements OnInit {
 
 	guessingWordAsString: string = 'jabłko';
 	guessingWord: GuessingLetter[] = [];
+	usedLetters: GuessingLetter[] = [];
 
 	mistakesCounter: number = 0;
-	level: number = 0;
+	level: number = 1;
 
-	timerValue: number = 0;
+	timerValue: number = 0; // in seconds
 	timerInterval: ReturnType<typeof setInterval> | null = null;
 
 	constructor(private _matDialog: MatDialog) {}
 
 	@HostListener('document:keydown', ['$event']) onKeydown(event: KeyboardEvent) {
-		this.onKeySelect(event.key);
+		if (!event.code.includes('Key') || this._matDialog.openDialogs.length) {
+			return;
+		}
+
+		this.onKeySelect(event.key.toUpperCase());
 	}
 
 	ngOnInit(): void {
@@ -100,10 +105,38 @@ export class AppComponent implements OnInit {
 	}
 
 	onKeySelect(key: string): void {
-		if (this.mistakesCounter === this.MAX_MISTAKES_NUMBER) {
+		if (this.mistakesCounter === this.MAX_MISTAKES_NUMBER || this.usedLetters.some((letter) => letter.char === key)) {
 			return;
 		}
 
+		const isLetterGuessed = this.isLetterGuessed(key);
+		const usedLetter: GuessingLetter = { char: key, isShow: isLetterGuessed };
+		this.usedLetters.push(usedLetter);
+
+		if (!isLetterGuessed) {
+			this.mistakesCounter =
+				this.mistakesCounter + 1 > this.MAX_MISTAKES_NUMBER ? this.MAX_MISTAKES_NUMBER : this.mistakesCounter + 1;
+
+			this.showGallowsSegment(this.mistakesCounter - 1);
+
+			if (this.mistakesCounter === this.MAX_MISTAKES_NUMBER) {
+				this.showStickman();
+
+				// show game over hangman after 1 sec
+				setTimeout(() => {
+					this.gameOver();
+				}, 1000);
+			}
+		} else if (
+			this.guessingWord.every((letter) => {
+				return letter.isShow;
+			})
+		) {
+			this.endLevel();
+		}
+	}
+
+	isLetterGuessed(key: string): boolean {
 		let isGuessed = false;
 
 		this.guessingWord.forEach((letter) => {
@@ -113,20 +146,7 @@ export class AppComponent implements OnInit {
 			}
 		});
 
-		if (!isGuessed) {
-			this.mistakesCounter =
-				this.mistakesCounter + 1 > this.MAX_MISTAKES_NUMBER ? this.MAX_MISTAKES_NUMBER : this.mistakesCounter + 1;
-
-			this.showGallowsSegment(this.mistakesCounter - 1);
-
-			if (this.mistakesCounter === this.MAX_MISTAKES_NUMBER) {
-				this.showStickman();
-
-				setTimeout(() => {
-					this.gameOver();
-				}, 1500);
-			}
-		}
+		return isGuessed;
 	}
 
 	showGallowsSegment(segmentNumber: number): void {
@@ -146,28 +166,67 @@ export class AppComponent implements OnInit {
 		(this.stickman[4].element as Path).d = 'M 90 75 l -7 23';
 		(this.stickman[5].element as Path).d = 'M 90 75 l 7 24';
 
-		this._matDialog
-			.open<InfoPopupComponent, InfoDialogData>(InfoPopupComponent, {
-				data: { title: 'Przegrałeś', content: `Szukane słowo to: ${this.guessingWordAsString}`, buttonText: 'Spróbuj ponownie' },
-				autoFocus: false,
-				position: { top: '50px' },
-			})
+		const dialogPopup: InfoDialogData = {
+			title: 'Przegrałeś',
+			content: `Szukane słowo to: ${this.guessingWordAsString}`,
+			buttonText: 'Spróbuj ponownie',
+		};
+
+		this.openPopup(dialogPopup)
 			.afterClosed()
 			.subscribe(() => {
 				this.resetGame();
 			});
 	}
 
+	endLevel(): void {
+		this.pauseTimer();
+
+		const isLastLvl = this.level === this.MAX_LEVEL;
+		let dialogPopup = {} as InfoDialogData;
+
+		if (isLastLvl) {
+			dialogPopup = {
+				title: 'Wygrałeś!',
+				content: `Zajeło ci to ${moment.utc(this.timerValue * 1000).format('mm:ss')}`,
+				buttonText: 'Zagraj jeszcze raz',
+			};
+		} else {
+			dialogPopup = {
+				title: 'Brawo!',
+				content: 'Możesz przejść do kolejnego poziomu.',
+				buttonText: `Poziom ${++this.level}`,
+			};
+		}
+
+		this.openPopup(dialogPopup)
+			.afterClosed()
+			.subscribe(() => {
+				this.resetGame(!isLastLvl);
+			});
+	}
+
+	openPopup(popupData: InfoDialogData): MatDialogRef<InfoPopupComponent, InfoDialogData> {
+		return this._matDialog.open<InfoPopupComponent, InfoDialogData>(InfoPopupComponent, {
+			data: { title: popupData.title, content: popupData.content, buttonText: popupData.buttonText },
+			autoFocus: false,
+			position: { top: '50px' },
+			disableClose: true,
+		});
+	}
+
 	resetGame(isNewLvl: boolean = false): void {
-		this.clearTimer();
 		this.prepareAnimation();
 		this.prepareGuessingWord(this.guessingWordAsString);
-		this.startTimer();
 		this.mistakesCounter = 0;
+		this.usedLetters = [];
 
-		if (isNewLvl) {
+		if (!isNewLvl) {
+			this.clearTimer();
 			this.level = 1;
 		}
+
+		this.startTimer();
 	}
 
 	getSegmentPath(segment: AnimationSegment): Path {
@@ -183,7 +242,7 @@ export class AppComponent implements OnInit {
 		(this.stickman[2].element as Path).d = 'M 90 60 l -15 10';
 		(this.stickman[3].element as Path).d = 'M 90 60 l 15 10';
 		(this.stickman[4].element as Path).d = 'M 75 93 l 15 -18';
-		(this.stickman[5].element as Path).d = 'M 90 75 l 15 1';
+		(this.stickman[5].element as Path).d = 'M 90 75 l 15 18';
 	}
 
 	setStickmanDead(): void {
@@ -192,6 +251,14 @@ export class AppComponent implements OnInit {
 		(this.stickman[3].element as Path).d = 'M 90 60 l 7 17';
 		(this.stickman[4].element as Path).d = 'M 90 75 l -7 23';
 		(this.stickman[5].element as Path).d = 'M 90 75 l 7 24';
+	}
+
+	onStickmanHelp(): void {
+		(this.stickman[0].element as Circle) = { cx: 90, cy: 48, r: 8 };
+		(this.stickman[2].element as Path).d = 'M 90 60 l -15 -10';
+		(this.stickman[3].element as Path).d = 'M 90 60 l 15 -10';
+		(this.stickman[4].element as Path).d = 'M 75 93 l 15 -18';
+		(this.stickman[5].element as Path).d = 'M 90 75 l 15 18';
 	}
 
 	startTimer(): void {
